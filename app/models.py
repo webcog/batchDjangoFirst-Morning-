9fab95a5888b50efe8b591335432a32be9a11dc9
Webcog <wajahatmurtaza144@gmail.com>
2023-08-27 12:10:56 +0500

    27-08-2023

--- app/models.py
+++ app/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    cnic = models.CharField(max_length=13)
+    address = models.TextField()
+    phone = models.IntegerField()
+
+    def __str__(self):
+        return str(self.name)
+        
+    
